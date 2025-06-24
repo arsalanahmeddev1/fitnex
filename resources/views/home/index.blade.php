5141c678facbd92c589bd7a221c8960d353fe197
unknown <production8411@gmail.com>
2025-06-24 22:01:38 +0500

    minor changes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -88,7 +88,7 @@
             </div>
         </div>
     </section>
-    <section class="custom-slider bg-black pt-[20px] pb-[100px]">
+    <section class="custom-slider bg-black pt-[20px] pb-[100px] relative z-[1]">
         <ul class="slider-01 bg-white py-[10px]">
             <li class="">
                 <span class="text-black">Personal Training</span>
