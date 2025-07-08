86021359ee3b406e732ae2638797c56fc9b3c485
unknown <production8411@gmail.com>
2025-07-08 23:16:52 +0500

    changes in master layout

--- resources/views/layouts/website/master.blade.php
+++ resources/views/layouts/website/master.blade.php
@@ -10,6 +10,7 @@
     <title>@yield('title')</title>
     <!-- Fonts -->
     <!-- Favicon -->
+     <!-- master layout -->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/assets/website') }}/images/logo.png">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/assets/website') }}/images/logo.png">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/website') }}/images/logo.png">
